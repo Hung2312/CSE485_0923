57609613c142327645956a46bc72310f0ebf334a
Hung2312 <145407643+Hung2312@users.noreply.github.com>
2023-09-19 08:42:07 +0700

    Add files via upload

---
+++ BTTH01_CSE485_ex04.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Bài 2</title>
+</head>
+<body>
+<?php
+$arrs = array(
+    "Italy" => "Rome",
+    "Luxembourg" => "Luxembourg",
+    "Belgium" => "Brussels",
+    "Denmark" => "Copenhagen",
+    "Finland" => "Helsinki",
+    "France" => "Paris",
+    "Slovakia" => "Bratislava",
+    "Slovenia" => "Ljubljana",
+    "Germany" => "Berlin",
+    "Greece" => "Athens",
+    "Ireland" => "Dublin",
+    "Netherlands" => "Amsterdam",
+    "Portugal" => "Lisbon",
+    "Spain" => "Madrid",
+    "Sweden" => "Stockholm",
+    "United Kingdom" => "London",
+    "Cyprus" => "Nicosia",
+    "Lithuania" => "Vilnius",
+    "Czech Republic" => "Prague",
+    "Estonia" => "Tallin",
+    "Hungary" => "Budapest",
+    "Latvia" => "Riga",
+    "Malta" => "Valetta",
+    "Austria" => "Vienna",
+    "Poland" => "Warsaw"
+);
+
+foreach ($arrs as $country => $capital) {
+    echo "Thủ đô của $country là $capital<br>";
+}
+?>
+
+</body>
+</html>
